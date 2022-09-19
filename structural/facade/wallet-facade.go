eec0a52a3f5610345e5ee8861f14119a8d9d1032
Zhao Shawn Young <shawnyoungzhao@gmail.com>
2022-09-20 01:43:18 +0800

    Finish pattern: Facade.

--- structural/facade/wallet-facade.go
+++ structural/facade/wallet-facade.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type WalletFacade struct {
 	account      *Account
@@ -36,5 +38,24 @@ func (w *WalletFacade) addMoneyToWallet(accountID string, securityCode int, amou
 	w.wallet.creditBalance(amount)
 	w.notification.sendWalletCreditNotification()
 	w.ledger.makeEntry(accountID, "credit", amount)
+	return nil
+}
+
+func (w *WalletFacade) deductMoneyFromWallet(accountID string, securityCode int, amount int) error {
+	fmt.Println("Starting debit money from wallet")
+	err := w.account.checkAccount(accountID)
+	if err != nil {
+		return err
+	}
+	err = w.securityCode.checkCode(securityCode)
+	if err != nil {
+		return err
+	}
+	err = w.wallet.debitBalance(amount)
+	if err != nil {
+		return err
+	}
+	w.notification.sendWalletDebitNotification()
+	w.ledger.makeEntry(accountID, "debit", amount)
 	return nil
 }
